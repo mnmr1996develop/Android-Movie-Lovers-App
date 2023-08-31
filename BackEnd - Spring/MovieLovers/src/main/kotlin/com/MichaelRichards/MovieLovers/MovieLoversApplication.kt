71ca7e7d448c40d668e1e7f01232f17b6449fda3
mnmr1996 <mnmr1996@gmail.com>
2023-08-31 13:32:05 -0400

    initializing frontend

---
+++ BackEnd - Spring/MovieLovers/src/main/kotlin/com/MichaelRichards/MovieLovers/MovieLoversApplication.kt
@@ -0,0 +1,11 @@
+package com.MichaelRichards.MovieLovers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieLoversApplication
+
+fun main(args: Array<String>) {
+	runApplication<MovieLoversApplication>(*args)
+}
---
+++ BackEnd - Spring/MovieLovers/src/test/kotlin/com/MichaelRichards/MovieLovers/MovieLoversApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.MichaelRichards.MovieLovers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieLoversApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
