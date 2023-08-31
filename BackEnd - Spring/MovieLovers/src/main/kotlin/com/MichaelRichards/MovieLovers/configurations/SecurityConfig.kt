caad1fddfeeda780568b4fba15e016c978b4b2b9
mnmr1996 <mnmr1996@gmail.com>
2023-08-31 14:21:56 -0400

    setting up jwt with a filter

--- BackEnd - Spring/MovieLovers/src/main/kotlin/com/MichaelRichards/MovieLovers/configurations/SecurityConfig.kt
+++ BackEnd - Spring/MovieLovers/src/main/kotlin/com/MichaelRichards/MovieLovers/configurations/SecurityConfig.kt
@@ -1,20 +1,41 @@
 package com.MichaelRichards.MovieLovers.configurations
 
+import com.MichaelRichards.MovieLovers.filters.JwtAuthenticationFilter
+import com.MichaelRichards.MovieLovers.services.EnthusiastService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 class SecurityConfig(
-
+    private val passwordEncoder: PasswordEncoder,
+    private val enthusiastService: EnthusiastService,
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
 ) {
 
+    @Bean
+    fun authenticationProvider(): AuthenticationProvider {
+        val authenticationProvider = DaoAuthenticationProvider()
+        authenticationProvider.setUserDetailsService(enthusiastService)
+        authenticationProvider.setPasswordEncoder(passwordEncoder)
+        return authenticationProvider
+    }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager = config.authenticationManager
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain = http
@@ -22,7 +43,9 @@ class SecurityConfig(
         .sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
         .authorizeHttpRequests { authorize ->
             authorize
-                .anyRequest().permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
+                .anyRequest().authenticated()
         }
+        .authenticationProvider(authenticationProvider()).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
         .build()
 }
---
+++ BackEnd - Spring/MovieLovers/src/main/kotlin/com/MichaelRichards/MovieLovers/services/JwtService.kt
@@ -0,0 +1,69 @@
+package com.MichaelRichards.MovieLovers.services
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Service
+import java.security.Key
+import java.util.*
+import java.util.function.Function
+
+@Service
+class JwtService {
+    companion object {
+        const val JWT_SECRET_KEY = "0af8dc2d7c5e0e167d723d75ab45b965b7b50839058371b2a1bafb0d727415e0"
+        const val JWT_EXPIRATION = 86400000L
+        const val JWT_REFRESH_EXPIRATION = 604800000L
+    }
+
+    fun extractUsername(token: String): String {
+        return extractClaim(token) { obj: Claims -> obj.subject }
+    }
+
+    fun generateToken(userDetails: UserDetails): String = generateToken(mutableMapOf(), userDetails)
+    fun generateRefreshToken(userDetails: UserDetails): String = buildToken(mutableMapOf(), userDetails, JWT_REFRESH_EXPIRATION)
+
+
+    fun isTokenValid(token: String, userDetails: UserDetails): Boolean {
+        val username = extractUsername(token)
+        return username == userDetails.username && !isTokenExpired(token)
+    }
+
+    private fun isTokenExpired(token: String): Boolean = extractExpiration(token).before(Date())
+
+    private fun extractExpiration(token: String): Date = extractClaim(token) { obj: Claims -> obj.expiration }
+
+
+    private fun <T> extractClaim(token: String, claimsResolvers: Function<Claims, T>): T {
+        val claims: Claims = extractAllClaims(token)
+        return claimsResolvers.apply(claims)
+    }
+
+    private fun generateToken(extraClaims: Map<String, Any>, userDetails: UserDetails): String = buildToken(extraClaims, userDetails, JWT_EXPIRATION)
+
+
+    private fun buildToken(extraClaims: Map<String, Any>, userDetails: UserDetails, expiration: Long) = Jwts
+        .builder()
+        .setClaims(extraClaims)
+        .setSubject(userDetails.username)
+        .setIssuedAt(Date(System.currentTimeMillis()))
+        .setExpiration(Date(System.currentTimeMillis() + expiration))
+        .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+        .compact()
+
+    private fun extractAllClaims(token: String): Claims = Jwts
+        .parserBuilder()
+        .setSigningKey(getSigningKey())
+        .build()
+        .parseClaimsJws(token)
+        .body
+
+
+    private fun getSigningKey(): Key {
+        val keyBytes = Decoders.BASE64.decode(JWT_SECRET_KEY)
+        return Keys.hmacShaKeyFor(keyBytes)
+    }
+}
