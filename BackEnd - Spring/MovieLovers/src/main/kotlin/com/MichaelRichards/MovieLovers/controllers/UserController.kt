4352d0c405dbebe208a934ef81e1998728a606be
mnmr1996 <mnmr1996@gmail.com>
2023-09-01 14:33:25 -0400

    using the bearer token to get user info in a controller

--- BackEnd - Spring/MovieLovers/src/main/kotlin/com/MichaelRichards/MovieLovers/controllers/UserController.kt
+++ BackEnd - Spring/MovieLovers/src/main/kotlin/com/MichaelRichards/MovieLovers/controllers/UserController.kt
@@ -1,16 +1,22 @@
 package com.MichaelRichards.MovieLovers.controllers
 
+import com.MichaelRichards.MovieLovers.models.Enthusiast
 import com.MichaelRichards.MovieLovers.services.EnthusiastService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("api/v1/users")
+@RequestMapping("/api/v1/users")
 class UserController(
     private val enthusiastService: EnthusiastService,
 ) {
-    @GetMapping("hello/")
-    fun sayHello() = "hello"
 
+    @GetMapping
+    fun getUserDetails(@RequestHeader("Authorization") bearerToken: String): ResponseEntity<Enthusiast>{
+        val enthusiast = enthusiastService.getUserByBearerToken(bearerToken)
+        return ResponseEntity.ok().body(enthusiast)
+    }
 }
--- BackEnd - Spring/MovieLovers/src/main/kotlin/com/MichaelRichards/MovieLovers/services/EnthusiastService.kt
+++ BackEnd - Spring/MovieLovers/src/main/kotlin/com/MichaelRichards/MovieLovers/services/EnthusiastService.kt
@@ -2,7 +2,6 @@ package com.MichaelRichards.MovieLovers.services
 
 import com.MichaelRichards.MovieLovers.models.Enthusiast
 import com.MichaelRichards.MovieLovers.repositories.EnthusiastRepository
-import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
@@ -11,7 +10,8 @@ import java.time.LocalDateTime
 
 @Service
 class EnthusiastService(
-    private val enthusiastRepository: EnthusiastRepository
+    private val enthusiastRepository: EnthusiastRepository,
+    private val jwtService: JwtService
 ) : UserDetailsService {
 
 
@@ -28,4 +28,11 @@ class EnthusiastService(
     }
 
     fun findByUsername(username: String): Enthusiast = enthusiastRepository.findByUsername(username) ?: throw UsernameNotFoundException("$username not found")
+    fun getUserByBearerToken(bearerToken: String): Enthusiast {
+        val jwt: String = bearerToken.substring(7)
+        val username = jwtService.extractUsername(jwt)
+        return findByUsername(username)
+    }
+
+    fun getAllUsers() : List<Enthusiast> = enthusiastRepository.findAll()
 }
